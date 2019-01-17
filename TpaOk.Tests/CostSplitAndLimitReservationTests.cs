cc7a5d917f63622a7da8d93621ddd319ebf9c673
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2019-01-17 10:04:16 +0100

    coverage check and empty copayment implemented

---
+++ TpaOk.Tests/CostSplitAndLimitReservationTests.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using NodaMoney;
+using TpaOk.Domain.Limits;
+using Xunit;
+
+namespace TpaOk.Tests
+{
+    public class CostSplitAndLimitReservationTests
+    {   
+        private CalculateCostSplitAndReserveLimitsCommandHandler cmdHandler =
+            new CalculateCostSplitAndReserveLimitsCommandHandler(new MockPolicyRepository());
+        
+        [Fact]
+        public void NoLimitsAndNoPreviousConsumption_TuPaysAll()
+        {
+            //given
+            var medCase = new Case
+            {
+                Number = "CASE_1",
+                InsuredId = 1,
+                PolicyId = 1,
+                Services = new List<CaseService>
+                {
+                    new CaseService
+                    {
+                        ServiceCode = "KONS_INTERNISTA",
+                        Date = new DateTime(2019,1,10),
+                        Price = Money.Euro(100),
+                        Qt = 1
+                    }
+                }
+            };
+
+            //when
+            var result = cmdHandler.Handle(new CalculateCostSplitAndReserveLimitsCommand(medCase));
+
+            //then
+            Assert.Equal(Money.Euro(0), result.InsuredCost);
+            Assert.Equal(Money.Euro(100), result.TuCost);
+            Assert.Equal(Money.Euro(100), result.TotalCost);
+            
+        }
+        
+        [Fact]
+        public void NoLimitsAndNoPreviousConsumptionButServiceNotCoverByPolicy_InsuredPaysAll()
+        {
+            //given
+            var medCase = new Case
+            {
+                Number = "CASE_1",
+                InsuredId = 1,
+                PolicyId = 1,
+                Services = new List<CaseService>
+                {
+                    new CaseService
+                    {
+                        ServiceCode = "KONS_INTERNISTA_X",
+                        Date = new DateTime(2019,1,10),
+                        Price = Money.Euro(100),
+                        Qt = 1
+                    }
+                }
+            };
+
+            //when
+            var result = cmdHandler.Handle(new CalculateCostSplitAndReserveLimitsCommand(medCase));
+
+            //then
+            Assert.Equal(Money.Euro(100), result.InsuredCost);
+            Assert.Equal(Money.Euro(0), result.TuCost);
+            Assert.Equal(Money.Euro(100), result.TotalCost);
+            
+        }
+        
+        [Fact]
+        public void NoLimitsAndNoPreviousConsumptionButInsuredNotCoverByPolicy_InsuredPaysAll()
+        {
+            //given
+            var medCase = new Case
+            {
+                Number = "CASE_1",
+                InsuredId = 2,
+                PolicyId = 1,
+                Services = new List<CaseService>
+                {
+                    new CaseService
+                    {
+                        ServiceCode = "KONS_INTERNISTA",
+                        Date = new DateTime(2019,1,10),
+                        Price = Money.Euro(100),
+                        Qt = 1
+                    }
+                }
+            };
+
+            //when
+            var result = cmdHandler.Handle(new CalculateCostSplitAndReserveLimitsCommand(medCase));
+
+            //then
+            Assert.Equal(Money.Euro(100), result.InsuredCost);
+            Assert.Equal(Money.Euro(0), result.TuCost);
+            Assert.Equal(Money.Euro(100), result.TotalCost);
+            
+        }
+        
+        [Fact]
+        public void NoLimitsAndNoPreviousConsumptionButNoPolicyVersionAtServiceDate_InsuredPaysAll()
+        {
+            //given
+            var medCase = new Case
+            {
+                Number = "CASE_1",
+                InsuredId = 2,
+                PolicyId = 2,
+                Services = new List<CaseService>
+                {
+                    new CaseService
+                    {
+                        ServiceCode = "KONS_INTERNISTA",
+                        Date = new DateTime(2019,1,10),
+                        Price = Money.Euro(100),
+                        Qt = 1
+                    }
+                }
+            };
+
+            //when
+            var result = cmdHandler.Handle(new CalculateCostSplitAndReserveLimitsCommand(medCase));
+
+            //then
+            Assert.Equal(Money.Euro(100), result.InsuredCost);
+            Assert.Equal(Money.Euro(0), result.TuCost);
+            Assert.Equal(Money.Euro(100), result.TotalCost);
+            
+        }
+    }
+}
