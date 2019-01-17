e10a3e637d531cffa677c019b8d5a03c6897b0af
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2019-01-17 22:49:10 +0100

    Consumption moved to separate file

--- TpaOk/Domain/Limits/CalculateCostSplitAndReserveLimitsCommandHandler.cs
+++ TpaOk/Domain/Limits/CalculateCostSplitAndReserveLimitsCommandHandler.cs
@@ -1,6 +1,3 @@
-using System;
-using NodaMoney;
-
 namespace TpaOk.Domain.Limits
 {
     public class CalculateCostSplitAndReserveLimitsCommandHandler
@@ -45,35 +42,6 @@ namespace TpaOk.Domain.Limits
 
 
             return costSplit;
-        }
-    }
-
-    public class Consumption
-    {
-        public int ConsumptionId { get; private set; }
-        public int PolicyId { get; private set; }
-        public int InsuredId { get; private set; }
-        public string CaseNumber { get; private set; }
-        public string ServiceCode { get; private set; }
-        public DateTime ConsumptionDate { get; private set; }
-        public Money ConsumedAmount { get; private set; }
-        public int ConsumedQuantity { get; private set; }
-        
-        
-        public Consumption(Case cmdCase, CaseService caseService, Money amountConsumed, int qtConsumed)
-            : this(cmdCase.PolicyId, cmdCase.InsuredId, cmdCase.Number, caseService.ServiceCode, caseService.Date,
-                amountConsumed, qtConsumed)
-        {}
-
-        public Consumption(int policyId, int insuredId, string caseNumber, string serviceCode, DateTime consumptionDate, Money consumedAmount, int consumedQuantity)
-        {
-            PolicyId = policyId;
-            InsuredId = insuredId;
-            CaseNumber = caseNumber;
-            ServiceCode = serviceCode;
-            ConsumptionDate = consumptionDate;
-            ConsumedAmount = consumedAmount;
-            ConsumedQuantity = consumedQuantity;
         }
     }
 }
